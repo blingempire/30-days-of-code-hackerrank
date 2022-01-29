2f681afb8a2aede59e2cf43f9823122598ce5ef3
Frank Lee <plat4m@outlook.com>
2022-01-29 15:19:32 -0800

    Solve - Day 2

--- src/Day2Operators.java
+++ src/Day2Operators.java
@@ -1,2 +1,40 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        // Write your code here
+
+        double tip = (meal_cost * tip_percent) / 100;
+        double tax = (meal_cost * tax_percent) / 100;
+        double totalCost = meal_cost + tip + tax;
+
+        System.out.println((int)Math.round(totalCost));
+    }
+
+}
+
 public class Day2Operators {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
 }
